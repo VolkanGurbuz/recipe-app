9dbc77a842e46c89ca5a3dbd8b8c3e8537e45df0
volkangurbuz <volkangurbuz16@gmail.com>
2020-04-28 11:49:01 +0200

    enumeration dropdowns added

--- src/main/java/com/volkangurbuz/controllers/RecipeController.java
+++ src/main/java/com/volkangurbuz/controllers/RecipeController.java
@@ -16,8 +16,8 @@ public class RecipeController {
   }
 
   // pathvariable annotions takes the id on the url that we can use
-  @GetMapping
-  @RequestMapping("/recipe/{id}/show")
+
+  @GetMapping("/recipe/{id}/show")
   public String showById(@PathVariable String id, Model model) {
 
     model.addAttribute("recipe", recipeService.findById(new Long(id)));
@@ -25,15 +25,13 @@ public class RecipeController {
     return "recipe/show";
   }
 
-  @GetMapping
-  @RequestMapping("recipe/new")
+  @GetMapping("recipe/new")
   public String newRecipe(Model model) {
     model.addAttribute("recipe", new RecipeCommand());
     return "recipe/recipeform";
   }
 
-  @GetMapping
-  @RequestMapping("recipe/{id}/update")
+  @GetMapping("recipe/{id}/update")
   public String updateRecipe(@PathVariable String id, Model model) {
 
     model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
@@ -49,8 +47,7 @@ public class RecipeController {
     return "redirect:/recipe/" + savedCommand.getId() + "/show";
   }
 
-  @GetMapping
-  @RequestMapping("recipe/{id}/delete")
+  @GetMapping("recipe/{id}/delete")
   public String deleteById(@PathVariable String id) {
     recipeService.deleteById(Long.valueOf(id));
 
