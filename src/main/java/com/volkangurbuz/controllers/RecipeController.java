2618b9fbe25260ef6e5f29cb329638fd5e5d5624
volkangurbuz <volkangurbuz16@gmail.com>
2020-04-28 20:41:00 +0200

    display validations errors

--- src/main/java/com/volkangurbuz/controllers/RecipeController.java
+++ src/main/java/com/volkangurbuz/controllers/RecipeController.java
@@ -7,14 +7,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
 public class RecipeController {
 
   private final RecipeService recipeService;
+  private static final String RECIPE_RECIPEFORM_URL = "recipe/recipeform";
 
   public RecipeController(RecipeService recipeService) {
     this.recipeService = recipeService;
@@ -38,15 +42,32 @@ public class RecipeController {
 
   @GetMapping("recipe/{id}/update")
   public String updateRecipe(@PathVariable String id, Model model) {
-
     model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
-    return "recipe/recipeform";
+    return RECIPE_RECIPEFORM_URL;
   }
 
   // after saving the saved recipe will be shown
-  @PostMapping
-  @RequestMapping("recipe")
-  public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+  // if the command failed so we have bad binding here, so our validation is failed
+  // it shows the orm again but we will have that modelattribute recipe there with fields error
+  // properties
+  @PostMapping("recipe")
+  public String saveOrUpdate(
+      @Valid @ModelAttribute("recipe") RecipeCommand command, BindingResult bindingResult) {
+
+    // bindingresult is result of the validation
+    // checking the bindingresult and asking it if it has errors.
+    if (bindingResult.hasErrors()) {
+
+      bindingResult
+          .getAllErrors()
+          .forEach(
+              objectError -> {
+                log.debug(objectError.toString());
+              });
+
+      return RECIPE_RECIPEFORM_URL;
+    }
+
     RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
     return "redirect:/recipe/" + savedCommand.getId() + "/show";
